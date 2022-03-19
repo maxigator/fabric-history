882513fdb2bbe0877e50ce55ca20deb68970c691
Maxigator <maxime@maximeurban.fr>
2022-03-19 20:20:28 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-
 /**
  * Override the initialize function for the _historyInit();
  */
-fabric.Canvas.prototype.initialize = (function(originalFn) {
-  return function(...args) {
+fabric.Canvas.prototype.initialize = (function (originalFn) {
+  return function (...args) {
     originalFn.call(this, ...args);
     this._historyInit(args.customProperties);
     return this;
@@ -13,8 +12,8 @@ fabric.Canvas.prototype.initialize = (function(originalFn) {
 /**
  * Override the dispose function for the _historyDispose();
  */
-fabric.Canvas.prototype.dispose = (function(originalFn) {
-  return function(...args) {
+fabric.Canvas.prototype.dispose = (function (originalFn) {
+  return function (...args) {
     originalFn.call(this, ...args);
     this._historyDispose();
     return this;
@@ -26,26 +25,26 @@ fabric.Canvas.prototype.dispose = (function(originalFn) {
  */
 fabric.Canvas.prototype._historyNext = function () {
   return JSON.stringify(this.toDatalessJSON(this.extraProps));
-}
+};
 
 /**
  * Returns current state of the string of the canvas
  */
 fabric.Canvas.prototype.customProperties = function () {
-  return (this.extraProps);
-}
+  return this.extraProps;
+};
 
 /**
  * Returns an object with fabricjs event mappings
  */
-fabric.Canvas.prototype._historyEvents = function() {
+fabric.Canvas.prototype._historyEvents = function () {
   return {
-    'object:added': this._historySaveAction,
-    'object:removed': this._historySaveAction,
-    'object:modified': this._historySaveAction,
-    'object:skewing': this._historySaveAction
-  }
-}
+    "object:added": this._historySaveAction,
+    "object:removed": this._historySaveAction,
+    "object:modified": this._historySaveAction,
+    "object:skewing": this._historySaveAction,
+  };
+};
 
 /**
  * Initialization of the plugin
@@ -55,33 +54,31 @@ fabric.Canvas.prototype._historyInit = function (customProperties) {
   this.historyRedo = [];
   this.extraProps = customProperties;
   this.historyNextState = this._historyNext();
-  
+
   this.on(this._historyEvents());
-}
+};
 
 /**
  * Remove the custom event listeners
  */
 fabric.Canvas.prototype._historyDispose = function () {
-  this.off(this._historyEvents())
-}
+  this.off(this._historyEvents());
+};
 
 /**
  * It pushes the state of the canvas into history stack
  */
 fabric.Canvas.prototype._historySaveAction = function () {
-
-  if (this.historyProcessing)
-    return;
+  if (this.historyProcessing) return;
 
   const json = this.historyNextState;
   this.historyUndo.push(json);
   this.historyNextState = this._historyNext();
-  this.fire('history:append', { json: json });
-}
+  this.fire("history:append", { json: json });
+};
 
 /**
- * Undo to latest history. 
+ * Undo to latest history.
  * Pop the latest state of the history. Re-render.
  * Also, pushes into redo history.
  */
@@ -96,11 +93,11 @@ fabric.Canvas.prototype.undo = function (callback) {
     // Push the current state to the redo history
     this.historyRedo.push(this._historyNext());
     this.historyNextState = history;
-    this._loadHistory(history, 'history:undo', callback);
+    this._loadHistory(history, "history:undo", callback);
   } else {
     this.historyProcessing = false;
   }
-}
+};
 
 /**
  * Redo to latest undo history.
@@ -115,46 +112,45 @@ fabric.Canvas.prototype.redo = function (callback) {
     // Every redo action is actually a new action to the undo history
     this.historyUndo.push(this._historyNext());
     this.historyNextState = history;
-    this._loadHistory(history, 'history:redo', callback);
+    this._loadHistory(history, "history:redo", callback);
   } else {
     this.historyProcessing = false;
   }
-}
+};
 
-fabric.Canvas.prototype._loadHistory = function(history, event, callback) {
+fabric.Canvas.prototype._loadHistory = function (history, event, callback) {
   var that = this;
 
-  this.loadFromJSON(history, function() {
+  this.loadFromJSON(history, function () {
     that.renderAll();
     that.fire(event);
     that.historyProcessing = false;
 
-    if (callback && typeof callback === 'function')
-      callback();
+    if (callback && typeof callback === "function") callback();
   });
-}
+};
 
 /**
  * Clear undo and redo history stacks
  */
-fabric.Canvas.prototype.clearHistory = function() {
+fabric.Canvas.prototype.clearHistory = function () {
   this.historyUndo = [];
   this.historyRedo = [];
-  this.fire('history:clear');
-}
+  this.fire("history:clear");
+};
 
 /**
  * Off the history
  */
-fabric.Canvas.prototype.offHistory = function() {
+fabric.Canvas.prototype.offHistory = function () {
   this.historyProcessing = true;
-}
+};
 
 /**
  * On the history
  */
-fabric.Canvas.prototype.onHistory = function() {
+fabric.Canvas.prototype.onHistory = function () {
   this.historyProcessing = false;
 
   this._historySaveAction();
-}
+};
